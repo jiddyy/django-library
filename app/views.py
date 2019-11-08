34e51ac593162de0f53ae5a6f91d517d398bb6a9
Student <student@donation-4.local>
2019-11-08 15:40:08 -0600

    completed 'lite' benchmark for backend django

--- app/views.py
+++ app/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Book, Transaction
+from datetime import datetime
+from django.contrib import messages
 
 # Create your views here.
+
+
+def home(request):
+    books = Book.objects.all()
+    return render(request, "book_list.html", {"books": books})
+
+
+def borrow_book(request, id):
+    book = Book.objects.get(id=id)
+    if book.in_stock == True:
+        book.in_stock = False
+        book.save()
+        transaction = Transaction.objects.create(
+            datetime=datetime.now(), action="CHECKOUT", book=book
+        )
+        messages.success(request, f"Borrowed {book.title} by {book.author}")
+        transaction.save()
+        return redirect("home")
+    else:
+        messages.error(request, f"{book.title} by {book.author} is unavailable")
+        return redirect("home")
+
+
+def return_book(request, id):
+    book = Book.objects.get(id=id)
+    if book.in_stock == False:
+        book.in_stock = True
+        book.save()
+        transaction = Transaction.objects.create(
+            datetime=datetime.now(), action="CHECKIN", book=book
+        )
+        messages.success(request, f"Returned {book.title} by {book.author}")
+        transaction.save()
+        return redirect("home")
+    else:
+        messages.error(request, f"{book.title} by {book.author} is already here")
+        return redirect("home")
--- library/urls.py
+++ library/urls.py
@@ -15,5 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import app.views
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", app.views.home, name="home"),
+    path("book/<id>/borrow", app.views.borrow_book, name="borrow_book"),
+    path("book/<id>/return", app.views.return_book, name="return_book")
+]
